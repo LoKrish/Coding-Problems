b3d555713d46dd90812b071002945cbcd791d5ba
LoKrish <62656579+LoKrish@users.noreply.github.com>
2020-11-06 09:33:11 +0530

    Update MaximumDepthOfBinaryTree.java

--- Tree/MaximumDepthOfBinaryTree.java
+++ Tree/MaximumDepthOfBinaryTree.java
@@ -1,15 +1,10 @@
-// CALCULATE MINIMUM DEPTH OF BINARY TREE
-public int minDepth(TreeNode root) {
+// CALCULATE MAXIMUM DEPTH OF BINARY TREE
+public int maxDepth(TreeNode root) {
         if(root==null)
             return 0;
         
-        int leftDepth=minDepth(root.left);
-        int rightDepth=minDepth(root.right);
+        int leftDepth=maxDepth(root.left);
+        int rightDepth=maxDepth(root.right);
         
-        if(leftDepth==0||rightDepth==0)
-        {
         return Math.max(leftDepth,rightDepth)+1;
-        }
-        
-        return Math.min(leftDepth,rightDepth)+1;
 }
